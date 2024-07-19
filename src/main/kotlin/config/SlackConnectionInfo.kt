27e7bcbfd0e553d2718c6c4dec572520b15d1299
Vikl5 <viktor.klindt@tietoevry.com>
2024-07-19 10:50:58 +0200

    Changed channelId to use an environmental var instead

--- src/main/kotlin/config/SlackConnectionInfo.kt
+++ src/main/kotlin/config/SlackConnectionInfo.kt
@@ -5,5 +5,5 @@ import com.slack.api.Slack
 data class SlackConnectionInfo(
     val client: Slack = Slack.getInstance(),
     val token: String = System.getenv("SLACK_BOT_TOKEN"),
-    val channelId: String = "C07BJDY1693"
+    val channelId: String = System.getenv("CHANNEL_ID")
 )
